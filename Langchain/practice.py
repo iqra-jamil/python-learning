4ad89f51dabab33fd1f66a2a6ff6a69d8be9616d
Iqra Jamil <iqraj690@gmail.com>
2026-09-17 19:22:37 +0500

    uncommented code

--- Langchain/practice.py
+++ Langchain/practice.py
@@ -1,45 +1,45 @@
 print("Langchain")
 
-# # from langchain_core.prompts import PromptTemplate
-# # # from langchain_openai import ChatOpenAI 
-# # from langchain_groq import ChatGroq
-# # from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+# from langchain_openai import ChatOpenAI 
+from langchain_groq import ChatGroq
+from langchain_core.output_parsers import StrOutputParser
 
-# # from dotenv import load_dotenv
-# # import os
-# # load_dotenv()
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 
-# # template="tell me a {adjective} joke about {topic}"
-# # promt_temp=PromptTemplate.from_template(template)
-# # # final_res=promt_temp.format(adjective="dark",topic="programming")
-# # # print(final_res)
-# # parser= StrOutputParser()
-# # model=ChatGroq(model="openai/gpt-oss-20b",api_key=os.getenv("GROQ_API_KEY"))
-# # chain=promt_temp|model|parser
-# # resposne=chain.invoke({"adjective":"dark","topic":"programming"})
-# # print(resposne)
+template="tell me a {adjective} joke about {topic}"
+promt_temp=PromptTemplate.from_template(template)
+# final_res=promt_temp.format(adjective="dark",topic="programming")
+# print(final_res)
+parser= StrOutputParser()
+model=ChatGroq(model="openai/gpt-oss-20b",api_key=os.getenv("GROQ_API_KEY"))
+chain=promt_temp|model|parser
+resposne=chain.invoke({"adjective":"dark","topic":"programming"})
+print(resposne)
 
-# #chat history for chatbots
-# from langchain_core.chat_history import InMemoryChatMessageHistory
-# from langchain.messages import HumanMessage,AIMessage
-# from langchain_groq import ChatGroq
-# from dotenv import load_dotenv
-# import os
-# load_dotenv()
+#chat history for chatbots
+from langchain_core.chat_history import InMemoryChatMessageHistory
+from langchain.messages import HumanMessage,AIMessage
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
-# history=InMemoryChatMessageHistory()
+history=InMemoryChatMessageHistory()
 
-# history.add_message(HumanMessage(content="hy i am iqra"))
+history.add_message(HumanMessage(content="hy i am iqra"))
 
-# model=ChatGroq(model="openai/gpt-oss-20b",api_key=os.getenv("GROQ_API_KEY"))
-# response=model.invoke(history.messages)
-# history.add_message(AIMessage(content=response.content))
-# print(response.content)
-# history.add_message(HumanMessage(content="what is my name"))
-# response=model.invoke(history.messages)
-# history.add_message(AIMessage(content=response.content))
-# print(response.content)
+model=ChatGroq(model="openai/gpt-oss-20b",api_key=os.getenv("GROQ_API_KEY"))
+response=model.invoke(history.messages)
+history.add_message(AIMessage(content=response.content))
+print(response.content)
+history.add_message(HumanMessage(content="what is my name"))
+response=model.invoke(history.messages)
+history.add_message(AIMessage(content=response.content))
+print(response.content)
 
 # Document Loader
 
